ba87bf65848349d70d7b0630fbf6c4e52a6b1f60
gledesma-misu <144293856+gledesma-misu@users.noreply.github.com>
2023-11-07 16:44:07 +0800

    Added sqlite

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,18 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
+const app = express();
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(cors());
 
-const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
+require("./routes")(app);
 
-app.post('/register', (req,res) => {
-    res.send({
-        message: `hello ${req.body.email}! registered`,
-
-    })
-})
-
-app.listen(process.env.PORT || 8081)
+sequelize.sync().then(() => {
+  app.listen(config.port);
+  console.log(`Started server ${config.port}`);
+});
